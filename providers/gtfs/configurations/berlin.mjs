e9d6a8ecd58a7c1062b3fb5daad59a163fbe7da2
tcr-n <erwan.delahaye@etu.univ-tlse2.fr>
2026-09-11 17:33:26 +0200

    ajout config berlin

---
+++ providers/gtfs/configurations/berlin.mjs
@@ -0,0 +1,21 @@
+/** @type {import('../src/model/source.ts').SourceOptions[]} */
+const sources = [
+	{
+		id: "vbb",
+		staticResourceHref: "https://unternehmen.vbb.de/gtfs",
+		realtimeResourceHrefs: [
+			"https://production.gtfsrt.vbb.de/",
+		],
+		excludeScheduled: false,
+		getNetworkRef: (journey) => journey?.trip.route.agency.id
+	},
+];
+
+/** @type {import('../src/configuration/configuration.ts').Configuration} */
+const configuration = {
+	id: "berlin",
+	computeDelayMs: 5_000,
+	sources,
+};
+
+export default configuration;
